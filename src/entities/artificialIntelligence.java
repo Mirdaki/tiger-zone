9c3136acde3f65e2084c9f35292cad5676bafa5c
Tana Konda <tana_konda@yahoo.com>
2016-11-28 17:28:41 -0500

    Update artificialIntelligence.java
    Added den prioritization, crocodile logic and retrieval/adding 2nd tiger/.

--- src/entities/artificialIntelligence.java
+++ src/entities/artificialIntelligence.java
@@ -17,10 +17,10 @@ public class artificialIntelligence {
 		moveCount = 0;
 	}
 
-	public ArrayList<Region> orderedListOfRegions(BoardObject aCurrentBoard) {
+	public ArrayList<Region> orderedListOfRegions() {
 		//currentBoard.allRegions;
 		ArrayList<Region> descendingRegions = new ArrayList<Region>();
-		for(Map.Entry<Integer, Region> entry : aCurrentBoard.allRegions.entrySet()) {
+		for(Map.Entry<Integer, Region> entry : currentBoard.incompleteRegions.entrySet()) {
 			Region tempRegion = entry.getValue();
 			//int pX = tempRegion.getPotential
 			descendingRegions.add(tempRegion);
@@ -28,10 +28,8 @@ public class artificialIntelligence {
 		//Collections.sort(descendingRegions, Collections.reverseOrder());
 		int n = descendingRegions.size();
        	Region temp;
-               
         for(int i=0; i < n; i++){
-            for(int j=1; j < (n-i); j++){
-                               
+            for(int j=1; j < (n-i); j++){          
                	if(descendingRegions.get(j-1).getPotential() < descendingRegions.get(j).getPotential()){
                     //swap the elements!
                     temp = descendingRegions.get(j-1);
@@ -47,7 +45,7 @@ public class artificialIntelligence {
 		int ourTigers = 0;
 		if(tempRegion.hasTigers()) {
 			for(int i = 0; i < tempRegion.getNumOfTigers(); i++) {
-				if(tempRegion.theTigers.get(i).owner == currentBoard.getPlayer(0)) {
+				if(tempRegion.theTigers.get(i).getTigerOwner() == currentBoard.getPlayer(0)) {
 					ourTigers++;
 				}
 			}
@@ -57,7 +55,7 @@ public class artificialIntelligence {
 		return theArray;
 
 	}
-
+/*
 	public boolean isAdjacent(Location ps, Location r)
 	{
 		if((ps.getX() == r.getX() && (Math.abs(ps.getY()- r.getY()) == 1)) || (ps.getY()==r.getY() && (Math.abs(ps.getX()-r.getX())==1))) 
@@ -69,86 +67,94 @@ public class artificialIntelligence {
 			return false;
 		}
 	}
-
+*/
 	public String getMove() {
 		Region tigerPlacer = null;
 		if(!currentBoard.canPlace(currentTile)) {
-		//iff we have 0 tigers left
-		//pass
-		//add another tiger
 			ourMove = "TILE " + currentTile.getType() + " ";
-
-//			if(currentBoard.getPlayer(0).getNumOfTigers() == 0) { 
-//				//pop out random tiger
-//				ArrayList<Region> otherTempArray = orderedListOfRegions(currentBoard);
-//				for(int i = otherTempArray.size() - 1; i >= 0; i--) {
-//					int tigers[] = checkOurTigers(otherTempArray.get(i));
-//					if(tigers[0] > 0) {
-//						//place Tiger
-//						for(int j = 0; j < otherTempArray.get(i).getNumOfTigers(); j++) {
-//							if(otherTempArray.get(i).theTigers.get(j).owner == currentBoard.getPlayer(0)) {
-//								Location thisisitTwo = otherTempArray.get(i).theTigers.get(j).getLocation();
-//								ourMove += "UNPLACEABLE RETRIEVE TIGER AT " + thisisitTwo.getX() + " " + thisisitTwo.getY();
-//								break;
-//							}
-//						}
-//						break;
-//					}
-//				}
-//			}
-//			else if(currentBoard.getPlayer(0).getNumOfTigers() > 1) {
-//				ArrayList<Region> tempArray = orderedListOfRegions(currentBoard);
-//				for(int i = 0; i < tempArray.size(); i++) {
-//
-//					int tigers[] = checkOurTigers(tempArray.get(i));
-//					if(tigers[1] - tigers[0] == 1) {
-//	
-//						//place Tiger
-//						for(int j = 0; j < tempArray.get(i).getNumOfTigers(); j++) {
-//
-//							if(tempArray.get(i).theTigers.get(j).owner == currentBoard.getPlayer(0)) {
-//
-//								Location thisisit = tempArray.get(i).theTigers.get(j).getLocation();
-//								ourMove += "UNPLACEABLE ADD ANOTHER TIGER TO" + " " + thisisit.getX() + " " + thisisit.getY();
-//								break;
-//							}
-//						}
-//						break;
-//					}
-//				}
-//			}
-//			else {
+			if(currentBoard.getPlayer(0).getNumOfTigers() == 0) { 
+			// pop out tiger we own from least valued incomplete region.
+				ArrayList<Region> dlist = orderedListOfRegions();
+				for(int i = dlist.size() - 1; i >= 0; i--) {	// starting from least valued region
+					int tigers[] = checkOurTigers(dlist.get(i));
+					if(tigers[0] > 0) {		// if we have tigers in it. 
+						//retrieve Tiger
+						for(int j = 0; j < dlist.get(i).getNumOfTigers(); j++) {
+							if(dlist.get(i).theTigers.get(j).getTigerOwner() == currentBoard.getPlayer(0)) {
+								Location removefrom = dlist.get(i).theTigers.get(j).getLocation();
+								ourMove += "UNPLACEABLE RETRIEVE TIGER AT " + removefrom.getX() + " " + removefrom.getY();
+								break;
+							}
+						}
+					break;
+				   }
+				}
+			}
+			else if(currentBoard.getPlayer(0).getNumOfTigers() > 1) {
+				ArrayList<Region> desclist = orderedListOfRegions();
+				for(int i = 0; i < desclist.size(); i++) {
+					int tigers[] = checkOurTigers(desclist.get(i));
+					if(tigers[1] - tigers[0] == 1) {
+						//place Tiger
+						for(int j = 0; j < desclist.get(i).getNumOfTigers(); j++) {
+							if(desclist.get(i).theTigers.get(j).getTigerOwner() == currentBoard.getPlayer(0)) {
+								Location addhere = desclist.get(i).theTigers.get(j).getLocation();
+								ourMove += "UNPLACEABLE ADD ANOTHER TIGER TO" + " " + addhere.getX() + " " + addhere.getY();
+							break;
+						}
+						}
+						break;
+					}
+				}
+			}
+			else {
 				ourMove += "UNPLACEABLE PASS";
-//			}
+     		}
 		}
 		else {
-			
-						
-
 			ourMove = "PLACE " + currentTile.getType() + " ";	
 			//BoardObject currentBoard = new BoardObject(currentBoard);
-			
-			//UNCOMMENT tempArray IF WE GET MERGE WORKING O(n^2)
-
-			//ArrayList<Region> tempArray = orderedListOfRegions(currentBoard);
+			//ArrayList<Region> tempArray = orderedListOfRegions();	// O(n^2) so if timeout deal with this.
 			ArrayList<TilePair> tempPS = currentBoard.getPossibleSpots();
-//			Map<Location, Integer> uniqueLocation = new HashMap<Location, Integer>();
-
-//			for(int i = 0; i < tempPS.size(); i++) {
-//				if(!uniqueLocation.containsKey(tempPS.get(i))) {
-//					uniqueLocation.put(tempPS.get(i).getLocation(), tempPS.get(i).getOrientation());
-//				}
-//				else 
-//				{
-//					int curVal = uniqueLocation.get(tempPS.get(i).getLocation());
-//					uniqueLocation.put(tempPS.get(i).getLocation(), curVal+1);
-//				}
-//			}
-			// Step 1 - pick tile.
-			
-			Random randomGenerator = new Random();
-			int index = randomGenerator.nextInt(tempPS.size());
-			
+			int index;
+			// Step 0 - override random if tile is a den tile. 
+			if(currentTile.getType().equals("Den"))
+			{
+				HashMap<Location, Integer> uniqueLocation = new HashMap<Location, Integer>();
+				for(int i = 0; i < tempPS.size(); i++) {
+					if(!uniqueLocation.containsKey(tempPS.get(i))) {
+						uniqueLocation.put(tempPS.get(i).getLocation(), 0);
+					}
+					else 
+					{
+						int curVal = uniqueLocation.get(tempPS.get(i).getLocation());
+						uniqueLocation.put(tempPS.get(i).getLocation(), curVal+1);
+					}
+				}
+				int maxMoore = -1;
+				Location bestloc;
+				for (HashMap.Entry<Location, Integer> entry : uniqueLocation.entrySet()) {
+				    Location location = entry.getKey();
+				    if(currentBoard.getMoore(location).size() > maxMoore)
+				    {
+				    	maxMoore = currentBoard.getMoore(location).size();
+				    	bestloc = location;
+				    }
+					}
+			   for (int i = 0; i < tempPS.size(); i++)
+			   {
+				   if(tempPS.get(i).getLocation()==bestloc)
+				   {
+					   index = i;
+				   }
+			   }
+			}
+			// Step 1 - pick random spot out of eligible ones.
+			else
+			{
+				Random randomGenerator = new Random();
+				index = randomGenerator.nextInt(tempPS.size());
+			}
 			TilePair tempSpot = tempPS.get(index);
 			Location tileLoc = tempSpot.getLocation();
 			int orientation = tempSpot.getOrientation();
@@ -158,23 +164,7 @@ public class artificialIntelligence {
 			
 			int adjustedY = BoardObject.startY + (BoardObject.COLSIZE/2 - row);
 			int adjustedX = BoardObject.startX + (col - BoardObject.ROWSIZE/2);
-			/*int printedOr;
-				switch(orientation)
-    			{
-     									case 0: 
-     										printedOr = 0;
-	   										break;
-     									case 1:
-     										printedOr = 90;
-     										break;
-     									case 2:
-     										printedOr = 180;
-     										break;
-     									case 3:
-     										printedOr = 270;
-     										break;
-     			}*/
-
+			
 			ourMove += "AT " + adjustedX + " " + adjustedY + " " + orientation; 
 			
 			currentTile.setOrientation(orientation / 90);
@@ -207,12 +197,27 @@ public class artificialIntelligence {
 			
 			if(maxPotential != -1) { 
 				if(currentBoard.getPlayer(0).getNumOfTigers() == 0) 
+				{
+					boolean placed = false;
+					if(currentBoard.getPlayer(0).getNumOfCrocs() == 0)
 					{
-						
-						
 						ourMove += " NONE";
-						
 					}
+					else
+					{
+						Region theregion = allRegions.get(potentialRegionID.get(maxIndex));
+							if(theregion.getType().equals("Lake"))
+							{
+								int tigers[] = checkOurTigers(theregion);
+								if(tigers[1] != 0 && tigers[0] == 0) {
+								// place croc
+								ourMove += " CROCODILE";	
+								placed = true;
+								break;
+								}
+							}
+					}
+				}
 				else
 				{
 					Region region = allRegions.get(potentialRegionID.get(maxIndex));
@@ -275,33 +280,9 @@ public class artificialIntelligence {
 //
 //			//check tiles in regions
 //			//if any playable spot is adjacent to tile in region place tile
-//
-//			if(currentBoard.getPlayer(0).getNumOfTigers() == 0) 
-//				{
-//					if(currentBoard.getPlayer(0).getNumOfCrocs() == 0)
-//					{
-//						ourMove += " NONE";
-//					}
-//					else
-//					{
-//						for(int i = 0; i < tempArray.size(); i++) {
-//							int tigers[] = checkOurTigers(tempArray.get(i));
-//							if(tigers[1] != 0 && tigers[0] == 0) {
-//								// place croc
-//								ourMove += " CROCODILE";
-//									
-//							break;
-//							}
-//						}
-//					}
-//				}
-//			else
-//				{
-//					// first legal?
-//					ourMove += " TIGER " + tigerPlacer.getRecentMin()); //decide region
-//				}
-		}
+		} 
+		
 		return ourMove;
-	}
+	} // end of get move
 
-}
+} // end of class
