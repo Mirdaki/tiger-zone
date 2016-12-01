cab62767b72d63a72680ae10a3bd756d6e5c1d9d
Connor Ward <c.ward@ufl.edu>
2016-12-01 18:21:13 -0500

    Uploading documentation from google drive and fitnesse wiki and fixture code Open port 12344 for UDP on all interfaces

---
+++ src/acceptanceTests/FixtureCode/animalFixture.java
@@ -0,0 +1,15 @@
+package tigerFixture;
+import entities.Animal;
+
+public class animalFixture {
+	String theType;
+	Animal testAnimal;
+	
+	public void setTypeTest(String nameType) {
+		theType = nameType;
+	}
+
+	public String getTypeTest() {
+		return theType;
+	}
+}
