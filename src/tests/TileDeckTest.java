2259df57ef6d4f2a9a08744a00a4684468177dfd
charleychau <charleychau81@gmail.com>
2016-12-02 17:26:15 -0500

    updated tileDeckTest and added TestRunner for command line use

--- src/tests/TileDeckTest.java
+++ src/tests/TileDeckTest.java
@@ -2,8 +2,11 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
+import entities.TigerTile;
 import entities.TileDeck;
 
 public class TileDeckTest {
@@ -18,17 +21,36 @@ public class TileDeckTest {
 	
 	
 	@Test
+	//Tests if a random tile was able to be pulled from the deck
 	public void tileGetRandomTest() {
 		TileDeck t = new TileDeck();
 		
 		assertEquals(0, t.getRandom().getOrientation());
 	}
+	
+	@Test
+	//Tests if start properly takes the start tile away from deck
+	public void startTest() {
+		TileDeck t = new TileDeck();
+		t.start("TLTJ-");
+		
+		assertEquals(76, t.getTiles().size());
+	}
 
 	@Test
-	public void tileGetNextTest() {
+	//Tests if tiledeck can be set and if getNext retrieves the next tile in the deck
+	public void tiledeckSetAndGetNextTest() {
 		TileDeck t = new TileDeck();
+		ArrayList<TigerTile> remainingTiles = new ArrayList<TigerTile>();
+		TigerTile t1 = new TigerTile("JJJJ-", 0);
+		TigerTile t2 = new TigerTile("JJJJX", 0);
+		remainingTiles.add(t1);
+		remainingTiles.add(t2);
 
-		assertEquals(false, t.getNext(1).isEmpty());
+		t.setActual(remainingTiles);
+
+		assertEquals("JJJJ-", t.getNext(0));
+		assertEquals("JJJJX", t.getNext(1));
 	}
 
 }
